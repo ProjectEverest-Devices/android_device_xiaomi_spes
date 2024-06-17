1818b60105ce4a16dabc8340434bb7a30d06bafb
Bruno Martins <bgcngm@gmail.com>
2024-06-17 21:59:49 +0800

    spes: Inherit from QTI compatibility matrix
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
