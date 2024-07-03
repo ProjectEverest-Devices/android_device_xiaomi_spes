6825d2c35401aef5e7149c86ada1187077e51a84
angelomds42 <angelomds42@gmail.com>
2024-07-02 22:47:19 -0300

    spes: Unset `NEED_AIDL_NDK_PLATFORM_BACKEND`
    Avoid conflicts when building with hardware/lineage/compat
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -242,9 +242,6 @@ VENDOR_SECURITY_PATCH := 2023-07-01
 # Sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
 
-# VNDK
-NEED_AIDL_NDK_PLATFORM_BACKEND := true
-
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
