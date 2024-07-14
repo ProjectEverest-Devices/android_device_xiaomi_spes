11d96d4c0033deb08941527a5fc4088a09199bf8
angelomds42 <angelomds42@gmail.com>
2024-07-14 01:58:48 -0300

    spes: Drop compatibility matrix
    Let's use common
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,6 @@ TARGET_OTA_ASSERT_DEVICE := spes,spesn
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
